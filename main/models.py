01cad78ff2381bc0c14ffb92dbab5e33c187e2dc
zeeshanshaikh07 <zeeshanshaikh790.zs@gmail.com>
2021-05-23 17:56:12 +0530

    models updated

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class news(models.Model):
+    nId = models.AutoField(primary_key=True)
+    nHeadline = models.CharField(max_length=100)
+    nBody = models.TextField()
+    nPic = models.ImageField(upload_to = 'news/')
+
+class blogs(models.Model):
+    bId = models.AutoField(primary_key=True)
+    bHeadline = models.CharField(max_length=100)
+    bBody = models.TextField()
+    bPic = models.ImageField(upload_to = 'blogs/')
